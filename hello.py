40bd5a76605e6a399f3410f50d417e7828744b06
seratonoia <hjizelle@gmail.com>
2024-08-27 21:52:13 -0600

    added 3 lines

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@ def main():
     print('Hello World')
     print('Hello World')
     print('Hello World')
+    print('Hello World')
+    print('Hello World')
+
     print('CS122: Intro to Python')
 
 if __name__== '__main__':
